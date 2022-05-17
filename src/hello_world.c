2e2c5d3d3bc00a36669aefb9d2ee639f95d7c526
KimSeoYe <kimseoye15@gmail.com>
2022-05-17 17:24:24 +0900

    Implement print_initial_script()

--- src/hello_world.c
+++ src/hello_world.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 
+void
+print_initial_scripts ()
+{
+	printf("This program is for testing GitHub Action and Git diff\n") ;
+}
+
 int
 main ()
 {
 	printf("Hello, world!\n") ;
+	print_initial_scripts() ;
 
 	return 0 ;
 }
