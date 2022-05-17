1d54a51d04d04f45cfe97c3b8c6229207d1a1b58
KimSeoYe <kimseoye15@gmail.com>
2022-05-17 19:13:21 +0900

    Modify print_initial_scripts() for test

--- test/hello_world.c
+++ test/hello_world.c
@@ -5,6 +5,7 @@ print_initial_scripts ()
 {
 	printf("This program is for testing GitHub Action and Git diff.\n") ;
 	printf("I want to find out what function is changed using git diff.\n") ;
+	printf("If the commit change is arise, Git Action will detect it.\n") ;
 }
 
 int
