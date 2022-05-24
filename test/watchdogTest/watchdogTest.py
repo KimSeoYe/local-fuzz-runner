2a15c634a81aed4232baeef3b9e185f0ba7c9698
KimSeoYe <kimseoye15@gmail.com>
2022-05-24 23:45:09 +0900

    Add a test for watchdog

---
+++ test/watchdogTest/watchdogTest.py
@@ -0,0 +1,24 @@
+import time
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+def on_created(event):
+    print("{event.src_path} has been created") 
+
+def main () :
+    event_handler = FileSystemEventHandler()
+    
+    path = "."
+    observer = Observer()
+    observer.schedule(event_handler, path, recursive=True)
+
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
+
+if __name__ == "__main__":
+    main()
