1f2c78218104845948dfd07dfc621ca6da8179cc
KimSeoYe <kimseoye15@gmail.com>
2022-05-28 00:18:22 +0900

    Need to implement report_issue()
    * It now get an exit condition

---
+++ githubUtils.py
@@ -0,0 +1,11 @@
+
+ISSUE_TITLE = "Automatically created issue from GitHub Action workflow"
+
+'''
+gh issue create --title "Issue title" --body "Issue body" --label "bug"  ...
+
+* use [const.OUT_DIR]/default/crash/[seed_name]
+'''
+def report_issue () :
+    
+    return
--- script.py
+++ script.py
@@ -5,6 +5,7 @@ import shutil
 import argparse
 
 import fuzzUtils as utils
+import githubUtils as gh
 import const
 
 def main () :
@@ -13,7 +14,7 @@ def main () :
     parser = argparse.ArgumentParser()  
     parser.add_argument("-w", help="path of working dir", required=True)
     parser.add_argument("-a", help="path of afl++", required=True) # Suppose : afl++ is already exist
-    parser.add_argument("-x", help="name of executable", required=True)
+    parser.add_argument("-x", help="path of executable (from working directory)", required=True)
     parser.add_argument("-o", help="path of origin seed dir", required=True)
     parser.add_argument("-p", help="path of per function seed dir", required=True)
     
@@ -21,7 +22,7 @@ def main () :
 
     wkdir_path = os.path.realpath(args.w)
     aflpp_path = os.path.realpath(args.a)
-    executable_name = args.x  # executable must be under the workdir
+    # executable_path = args.x  # executable must be under the workdir
     origin_seed_dir = os.path.realpath(args.o)
     per_func_seed_dir = os.path.realpath(args.p)
     
@@ -30,15 +31,20 @@ def main () :
     # Build fuzzers
     build_cmd = "CC=" + aflpp_path + "/afl-cc make"
     os.system(build_cmd)
+    executable_path = os.path.realpath(args.x) # TODO check if correct
 
     # Get a new seed directory
     changed_funcs = utils.get_changed_func_names(wkdir_path)
     local_seeddir_path = utils.get_seeds_for_local_mode(origin_seed_dir, per_func_seed_dir, changed_funcs)
     
     # Run afl++ and monitoring crash directory
-    return_code = utils.execute_aflpp(aflpp_path, executable_name, local_seeddir_path, False) # TODO get is_file as an argument
-    print("\nRETURN CODE = ", return_code, type(return_code))
-
+    exit_cond = utils.execute_aflpp(aflpp_path, executable_path, local_seeddir_path, False) # TODO get is_file as an argument
+    
+    if exit_cond == const.ExitCond.TIMEOUT :
+        print("TIMEOUT")
+    elif exit_cond == const.ExitCond.CRASH :
+        gh.report_issue()
+    
     # Remove new seed directory and output directory
     shutil.rmtree(local_seeddir_path)
     shutil.rmtree(const.OUT_DIR)
